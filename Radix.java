bffc34de707c71b4ab8b45959eacb200c78a918e
Michael <mlin01@stuy.edu>
2019-04-01 21:48:37 -0400

    working on combining buckets

--- Radix.java
+++ Radix.java
@@ -1,16 +1,31 @@
+import java.util.*;
+
 public class Radix{
   //extend array to include negatives
   @SuppressWarnings("unchecked")
   public static void radixsort(int[] data){
-    MyLinkedList<Integer>[] buckets = new MyLinkedList<Integer>[10];
+    int max = data[0];
+    for(int i: data){
+      if(i>max) max = i;
+    }
+
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
     for(int i = 0; i < buckets.length;i++){
       buckets[i] = new MyLinkedList<Integer>();
     }
-    int place = 1;
-    for(int i = 0; i < data.length;i++){
-      if(data[i]/(place)>0){
-        buckets[data[i]/place%10].addEnd(data[i]);
+    for(int place = 1; max/place > 0; place*=10){
+      for(int i = 0; i < data.length;i++){
+        //if number's nth place > 0
+        if(data[i]/(place)>0){
+          //add to appropriate bucket
+          buckets[data[i]/place%10].addEnd(data[i]);
+        }
       }
     }
+  }
+
+  public static void main(String args[]){
+    int[] data = new int[10];
+    radixsort(data);
   }
 }

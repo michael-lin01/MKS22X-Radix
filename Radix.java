ace8c267fa6e678722efd04cf7e7f0c358c4642d
Michael <mlin01@stuy.edu>
2019-04-02 18:35:39 -0400

    tested radix

--- Radix.java
+++ Radix.java
@@ -4,9 +4,9 @@ public class Radix{
   //extend array to include negatives
   @SuppressWarnings("unchecked")
   public static void radixsort(int[] data){
-    int max = data[0];
+    int max = Math.abs(data[0]);
     for(int i: data){
-      if(i>max) max = i;
+      if(Math.abs(i)>max) max = Math.abs(i);
     }
 
     MyLinkedList<Integer>[] buckets = new MyLinkedList[19];
@@ -21,22 +21,17 @@ public class Radix{
 
     for(int place = 1; max/place > 0; place*=10){
       for(int i = 0; i < data.length;i++){
-        int front = temp.removeFront();
+        int n = temp.removeFront();
         //add to appropriate bucket
-        /*
-        System.out.println(front);
-        System.out.println(temp);
-        System.out.println("place: "+front/place%10);
-        */
-        if(front >= 0) buckets[9+front/place%10].addEnd(front);
-        else buckets[9+front/place%10].addFront(front);
+        buckets[9+n/place%10].addEnd(n);
       }
       for(int i = 0; i < buckets.length;i++){
-        //System.out.println(buckets[i]);
-        //System.out.println(temp);
-        if(buckets[i].size()>0) temp.extend(buckets[i]);
+        //System.out.println("bucket "+i+": "+buckets[i]);
+        if(buckets[i].size()>0) {
+          temp.extend(buckets[i]);
+          //System.out.println("temp: "+temp);
+        }
       }
-      //System.out.println( ""+place+" pass: "+temp);
     }
     for(int i = 0; i < data.length;i++){
       data[i]=temp.removeFront();
@@ -44,7 +39,8 @@ public class Radix{
   }
 
   public static void main(String args[]){
-    int[] data = {2,3,5,10,2,15,25};
+    System.out.println(Math.pow(-1,(1+(int)(Math.random()*2))));
+    int[] data = {2,3,5,10,2,15,25,-12,-19,-99,-109,-20};
     radixsort(data);
     System.out.println(Arrays.toString(data));
   }
